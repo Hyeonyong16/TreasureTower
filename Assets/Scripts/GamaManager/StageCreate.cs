c1785daa3eca8cb7546fb47fad6a542222cd4998
Hyeonyong16 <Hyeonyong16@users.noreply.github.com>
2020-11-15 06:09:19 +0900

    4차 업로드 20/11/15
    스테이지 생성
    플레이어 주변의 스테이지만 보이는 형식으로 변경
    테스트용 bool변수 추가
    -체크하면 현재 스테이지의 모든 방 확인

--- Assets/Scripts/GamaManager/StageCreate.cs
+++ Assets/Scripts/GamaManager/StageCreate.cs
@@ -4,14 +4,15 @@ using UnityEngine;
 
 public class StageCreate : MonoBehaviour
 {
-    public GameObject Room1;
-    public GameObject Room2;
-    public GameObject Room3;
-    public GameObject Room4;
+    public bool checkTest = false;
 
-    public GameObject StartZone;
-    public GameObject FinishZone;
-    public GameObject Shop;
+    public GameObject StartZone;    //0
+    public GameObject Room1;        //1
+    public GameObject Room2;        //2
+    public GameObject Room3;        //3
+    public GameObject Room4;        //4
+    public GameObject Shop;         //5
+    public GameObject FinishZone;   //6
 
     public float distance;
 
@@ -20,11 +21,13 @@ public class StageCreate : MonoBehaviour
     public int[] roomSetList;  //방목록
     public GameObject[] roomPrefabList;    //바닥에 깔은 방 프리팹목록
 
+    public Vector2 _PlayerPos;
+
     // Start is called before the first frame update
     void Start()
     {
-        roomSetList = new int[(stage+4)*(stage+4)];
-        roomPrefabList = new GameObject[(stage + 4)*(stage + 4)];
+        roomSetList = new int[(stage + 4) * (stage + 4)];
+        roomPrefabList = new GameObject[(stage + 4) * (stage + 4)];
 
         SetStageRoom(Random.Range(0, 10000));
     }
@@ -32,7 +35,7 @@ public class StageCreate : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        CreatePosRoom(_PlayerPos);
     }
 
     void SetStageRoom(int seed)
@@ -43,12 +46,13 @@ public class StageCreate : MonoBehaviour
         int shopNum = 0;
 
 
-        for(int i = 0; i < (stage+4)*(stage+4); i++)
+        for (int i = 0; i < (stage + 4) * (stage + 4); i++)
         {
             int temp = Random.Range(1, 6);
-            if(temp == 5)
+            if (temp == 5)
             {
-                if (shopNum >= shopMax) {
+                if (shopNum >= shopMax)
+                {
                     temp = Random.Range(1, 5);
                 }
 
@@ -78,9 +82,9 @@ public class StageCreate : MonoBehaviour
 
     void SetRoomList()
     {
-        for(int i = 0; i < stage+4; i++)
+        for (int i = 0; i < stage + 4; i++)
         {
-            for(int j = 0; j < stage+4; j++)
+            for (int j = 0; j < stage + 4; j++)
             {
                 switch (roomSetList[((stage + 4) * i) + j])
                 {
@@ -106,7 +110,100 @@ public class StageCreate : MonoBehaviour
                         roomPrefabList[((stage + 4) * i) + j] = Instantiate(FinishZone, new Vector3((i * distance), 0, (j * distance)), Quaternion.identity);
                         break;
                 }
+                roomPrefabList[((stage + 4) * i) + j].transform.SetParent(GameObject.Find("StageRooms").gameObject.transform);
+                roomPrefabList[((stage + 4) * i) + j].SetActive(false);
+
             }
+        }
+    }
+
+    public void CreatePosRoom(Vector2 playerPos)
+    {
+        int x = (int)playerPos.x;
+        int y = (int)playerPos.y;
+        bool[] roomActiveCheck = new bool[(stage+4)*(stage+4)];
+
+        for (int i = 0; i < (stage + 4) * (stage + 4); i++)
+            roomActiveCheck[i] = false;
+
+        roomActiveCheck[((stage + 4) * y) + x] = true;
+
+        if (x == 0)
+        {
+            if (y == 0)
+            {
+                roomActiveCheck[((stage + 4) * (y+1)) + x] = true;
+                roomActiveCheck[((stage + 4) * y) + (x+1)] = true;
+            }
+
+            else if (y == (stage + 3))
+            {
+                roomActiveCheck[((stage + 4) * (y-1)) + x] = true;
+                roomActiveCheck[((stage + 4) * y) + (x+1)] = true;
+            }
+
+            else
+            {
+                roomActiveCheck[((stage + 4) * (y-1)) + x] = true;
+                roomActiveCheck[((stage + 4) * y) + (x+1)] = true;
+                roomActiveCheck[((stage + 4) * (y+1)) + x] = true;
+            }
+        }
+
+        else if(x == (stage + 3))
+        {
+            if (y == 0)
+            {
+                roomActiveCheck[((stage + 4) * (y+1)) + x] = true;
+                roomActiveCheck[((stage + 4) * y) + (x-1)] = true;
+            }
+
+            else if (y == (stage + 3))
+            {
+                roomActiveCheck[((stage + 4) * y) + (x-1)] = true;
+                roomActiveCheck[((stage + 4) * (y-1)) + x] = true;
+            }
+
+            else
+            {
+                roomActiveCheck[((stage + 4) * (y-1)) + x] = true;
+                roomActiveCheck[((stage + 4) * y) + (x-1)] = true;
+                roomActiveCheck[((stage + 4) * (y+1)) + x] = true;
+            }
+        }
+
+        else//여기고칠차례
+        {
+            if (y == 0)
+            {
+                roomActiveCheck[((stage + 4) * y) + (x-1)] = true;
+                roomActiveCheck[((stage + 4) * y) + (x+1)] = true;
+                roomActiveCheck[((stage + 4) * (y+1)) + x] = true;
+            }
+
+            else if (y == (stage + 3))
+            {
+                roomActiveCheck[((stage + 4) * y) + (x - 1)] = true;
+                roomActiveCheck[((stage + 4) * y) + (x + 1)] = true;
+                roomActiveCheck[((stage + 4) * (y-1)) + x] = true;
+            }
+
+            else
+            {
+                roomActiveCheck[((stage + 4) * y) + (x - 1)] = true;
+                roomActiveCheck[((stage + 4) * (y - 1)) + x] = true;
+                roomActiveCheck[((stage + 4) * y) + (x + 1)] = true;
+                roomActiveCheck[((stage + 4) * (y + 1)) + x] = true;
+            }
+        }
+
+        for (int i = 0; i < (stage + 4) * (stage + 4); i++)
+        {
+            if(checkTest)
+                roomPrefabList[i].SetActive(true);
+
+            else
+                roomPrefabList[i].SetActive(roomActiveCheck[i]);
         }
     }
 }
